14f6c1f80f0769bd1293192c12652b732dd2f5b8
LiuChunfu <liuchunfu@outlook.com>
2018-10-03 00:04:35 +0800

    init...

---
+++ src/main/java/com/wangwenjun/metrics/metric/histograms/SlidingWindowReservoirHistogramExample.java
@@ -0,0 +1,42 @@
+package com.wangwenjun.metrics.metric.histograms;
+
+import com.codahale.metrics.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+/***************************************
+ * 最适合的是最近一段时间的数据分布图
+ * @author:Alex Wang <br/>
+ * @taobao:http://wangwenjun0609.taobao.com
+ */
+public class SlidingWindowReservoirHistogramExample {
+    private final static MetricRegistry registry = new MetricRegistry();
+    private final static ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.SECONDS)
+            .build();
+
+    private final static Histogram histogram = new Histogram(new SlidingWindowReservoir(50));
+
+    public static void main(String[] args) {
+        reporter.start(10, TimeUnit.SECONDS);
+        registry.register("SlidingWindowReservoir-Histogram", histogram);
+        while (true) {
+            doSearch();
+            randomSleep();
+        }
+    }
+
+    private static void doSearch() {
+        histogram.update(ThreadLocalRandom.current().nextInt(10));
+    }
+
+    private static void randomSleep() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/wangwenjun/metrics/metric/histograms/UniformReservoirHistogramExample.java
@@ -0,0 +1,45 @@
+package com.wangwenjun.metrics.metric.histograms;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.UniformReservoir;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+/***************************************
+ * 适合段一段非常长的时间，不适合已知的数据最近发生了变化的情况。
+ * @author:Alex Wang <br/>
+ * @taobao:http://wangwenjun0609.taobao.com
+ ***************************************/
+public class UniformReservoirHistogramExample {
+    private final static MetricRegistry registry = new MetricRegistry();
+    private final static ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.SECONDS)
+            .build();
+
+    private final static Histogram histogram = new Histogram(new UniformReservoir());
+
+    public static void main(String[] args) {
+        reporter.start(10, TimeUnit.SECONDS);
+        registry.register("UniformReservoir-Histogram", histogram);
+        while (true) {
+            doSearch();
+            randomSleep();
+        }
+    }
+
+    private static void doSearch() {
+        histogram.update(ThreadLocalRandom.current().nextInt(10));
+    }
+
+    private static void randomSleep() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
